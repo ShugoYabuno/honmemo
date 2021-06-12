835a9aa4909a0cb83445ae0bf2ca0363d237ec76
ShugoYabuno <shugo.ybn@gmai.com>
2021-06-12 18:00:30 +0900

    update: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,15 @@ module.exports = {
     browser: true,
     node: true
   },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    sourceType: 'module'
-  },
-  extends: ['eslint:recommended', '@vue/typescript', 'prettier'],
+  // parser: '@typescript-eslint/parser',
+  // parserOptions: {
+  //   sourceType: 'module'
+  // },
+  extends: [
+    'eslint:recommended',
+    '@vue/typescript',
+    'plugin:prettier/recommended'
+  ],
   plugins: ['@typescript-eslint'],
   // add your custom rules here
   rules: {}
