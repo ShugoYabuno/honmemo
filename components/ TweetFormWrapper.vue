a1c8fd12048eaa33da53c20c3b5b78909d9e9ea0
ShugoYabuno <shugo.ybn@gmai.com>
2021-07-26 12:33:36 +0900

    update: 新規投稿画面切り出し

---
+++ components/ TweetFormWrapper.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="tweet-form-wrapper">
+    <slot></slot>
+    <div class="tweet-form">
+      <SearchBooksForm />
+      <div class="tweet-button">
+        <v-btn fab>
+          <v-icon>mdi-pencil-plus</v-icon>
+        </v-btn>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,14 @@
 <template>
   <v-container class="py-8 px-6" fluid>
-    <v-row>
-      <v-col>
-        <SearchBooksForm />
-      </v-col>
-      <!-- <v-col v-for="card in cards" :key="card" cols="12">
-        <v-card>
-          <v-subheader>{{ card }}</v-subheader>
-
-          <v-list two-line>
-            <template v-for="n in 6">
-              <v-list-item :key="n">
-                <v-list-item-avatar color="grey darken-1"> </v-list-item-avatar>
-
-                <v-list-item-content>
-                  <v-list-item-title>Message {{ n }}</v-list-item-title>
-
-                  <v-list-item-subtitle>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Nihil repellendus distinctio similique
-                  </v-list-item-subtitle>
-                </v-list-item-content>
-              </v-list-item>
-
-              <v-divider v-if="n !== 6" :key="`divider-${n}`" inset></v-divider>
-            </template>
-          </v-list>
-        </v-card>
-      </v-col> -->
-    </v-row>
+    <!-- <SearchBooksForm />
+    <div class="pencil-plus">
+      <v-btn fab>
+        <v-icon>mdi-pencil-plus</v-icon>
+      </v-btn>
+    </div> -->
+    <TweetFormWrapper>
+      <h1>テスト</h1>
+    </TweetFormWrapper>
   </v-container>
 </template>
 
